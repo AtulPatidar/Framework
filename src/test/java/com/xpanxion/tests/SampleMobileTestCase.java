b491c04ac4a5f74b891916f3a24d55ec7aed2461
Chaitanya Dharmadhikari <cdharmadhikari@xpanxion.co.in>
2017-03-03 17:25:14 +0530

    Added sample mobile web tests with page Object model

--- src/test/java/com/xpanxion/tests/SampleMobileTestCase.java
+++ src/test/java/com/xpanxion/tests/SampleMobileTestCase.java
@@ -1,27 +1,26 @@
 package com.xpanxion.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 import com.xpanxion.base.DriverFactory;
 import com.xpanxion.core.CoreTestCase;
 import com.xpanxion.core.MobileTypes;
 import com.xpanxion.dataproviders.DataProviderLibrary;
+import com.xpanxion.pages.MobileHomeScreen;
 
 public class SampleMobileTestCase extends CoreTestCase {
 
     @Test(dataProvider = DataProviderLibrary.DP_GENERIC_MOBILE_NATIVE, dataProviderClass = DataProviderLibrary.class)
     public void testAppLaunch(MobileTypes types) {
-        DriverFactory.registerInstance(types.getDriverInstance());
-        WebDriver driver = DriverFactory.getDriverInstance();
+    	DriverFactory.registerInstance(types.getDriverInstance());
+    	MobileHomeScreen sample = new MobileHomeScreen();
         try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        String loc = driver.findElement(By.id("com.weather.Weather:id/location_actionbar_name")).getText();
+        String loc = sample.getLocationName();
         System.out.println(loc);
     }
 
