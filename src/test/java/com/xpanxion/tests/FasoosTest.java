f0f88f4db8659419ecdec8c615fe5c22d9ce3c90
Devraj <devrajh@xpanxion.co.in>
2017-03-03 13:54:17 +0530

    Added new test

---
+++ src/test/java/com/xpanxion/tests/FasoosTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xpanxion.tests;
+
+import com.xpanxion.base.DriverFactory;
+import com.xpanxion.core.BrowserTypes;
+import com.xpanxion.core.CoreTestCase;
+import com.xpanxion.dataproviders.DataProviderLibrary;
+import javax.swing.JOptionPane;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author xpanxion
+ */
+public class FasoosTest extends CoreTestCase {
+
+    //@Test
+    public void verifySearchItem() {
+        WebDriver driver = DriverFactory.getDriverInstance();
+        driver.get("https://order.faasos.io/");
+    }
+
+    @Test(dataProvider = DataProviderLibrary.DP_SEARCHFOODITEM, dataProviderClass = DataProviderLibrary.class)
+    public void verifySearchItem(String searchItem) {
+        WebDriver driver = DriverFactory.getDriverInstance();
+        driver.get("https://order.faasos.io/");
+        JOptionPane.showInputDialog(searchItem);
+    }
+
+}
